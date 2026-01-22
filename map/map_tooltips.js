0a6836410d9f5fe83e0aa7d9ac8d139ac6f3cb69
jgaffuri <jgaf@proton.me>
2026-01-22 14:55:34 +0100

    upgrade

--- map/map_tooltips.js
+++ map/map_tooltips.js
@@ -86,17 +86,16 @@ const getTooltipDemography = (mapCode) => {
         if (mapCode == "dep_ratio") line += " age (65+) or (0-14) per 100 age (15-64)"
         if (mapCode == "oa_dep_ratio") line += " age (65+) per 100 age (15-64)"
         if (mapCode == "y_dep_ratio") line += " age (0-14) per 100 age (15-64)"
-        if (mapCode == "median_age") line += " years"
+        if (mapCode == "median_age") line += " years (median age estimate)"
         buf.push(line)
 
         let total = c.Y_LT15 + c.Y_1564 + c.Y_GE65;
-        //if (isNaN(total)) total = c.T;
-        const tot_ = formatLarge(total) + " person" + (total == 1 ? "" : "s");
+        const tot_ = formatLarge(total) + " person" + (total == 1 ? "" : "s in total");
         buf.push(tot_)
 
-        buf.push(c.Y_LT15 + " - under 15 years")
-        if (mapCode != "ageing") buf.push(c.Y_1564 + " - 15 to 64 years")
-        buf.push(c.Y_GE65 + " - 65 years and older")
+        if (mapCode != "oa_dep_ratio") buf.push("Age under 15 years: " + c.Y_LT15)
+        if (mapCode != "ageing") buf.push("Age 15 to 64 years: " + c.Y_1564)
+        if (mapCode != "y_dep_ratio") buf.push("Age 65 years and older: " + c.Y_GE65)
         return buf.join("<br>");
     }
 }
