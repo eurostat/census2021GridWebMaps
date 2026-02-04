a3bafd1bc3fe09e3c9d39ea6ac3a656cb675d20c
jgaffuri <jgaf@proton.me>
2026-02-04 20:24:55 +0100

    upgrade

--- map_total/map.js
+++ map_total/map.js
@@ -69,37 +69,9 @@ const update = () => {
 
     // set dataset
     gridLayer.dataset = dataset[year]
-
-    //get colors
-    const colors = []
-    const classNumber = 8;
-    for (let i = 0; i <= (classNumber - 1); i++) colors.push(d3.interpolateYlOrRd(0.85 * i / (classNumber - 1)))
-    const scaleTPop = gridviz.exponentialScale(7)
-    const popCols = { ...colors }; popCols.na = naColor
-
-    //style
-    let totPopStyle = new gridviz.SquareColorCategoryWebGLStyle({
-        viewScale: (cells, r) => {
-            const max = d3.max(cells, c => c.p)
-            const rr = r * r / 1000000
-            const breaks = []
-            for (let i = 1; i < classNumber; i++) {
-                let t = i / classNumber
-                t = scaleTPop(t)
-                breaks.push(max * t / rr)
-            }
-            return gridviz.classifier(breaks)
-        },
-        code: (c, r, z, classifier) => {
-            const v = c.p
-            if (v == -1 || v == undefined) return "na"
-            return classifier(1000000 * v / r / r)
-        },
-        color: popCols,
-    })
-
     // set style
     gridLayer.styles = [totPopStyle, strokeStyle]
+    gridLayer.minPixelsPerCell = 0.7;
 
 
     //redraw
--- map_total/map_styles.js
+++ map_total/map_styles.js
@@ -3,3 +3,36 @@ const naColor = "#ccc";
 
 // default stroke style
 const strokeStyle = new gridviz.StrokeStyle({ strokeColor: () => "#fff8", visible: (z) => z < 15, blendOperation: () => "source-over" });
+
+
+
+// color style
+
+//get colors
+const colors = []
+const classNumber = 8;
+for (let i = 0; i <= (classNumber - 1); i++) colors.push(d3.interpolateYlOrRd(0.85 * i / (classNumber - 1)))
+const scaleTPop = gridviz.exponentialScale(7)
+const popCols = { ...colors }; popCols.na = naColor
+
+//style
+let totPopStyle = new gridviz.SquareColorCategoryWebGLStyle({
+    viewScale: (cells, r) => {
+        const max = d3.max(cells, c => c.p)
+        const rr = r * r / 1000000
+        const breaks = []
+        for (let i = 1; i < classNumber; i++) {
+            let t = i / classNumber
+            t = scaleTPop(t)
+            breaks.push(max * t / rr)
+        }
+        return gridviz.classifier(breaks)
+    },
+    code: (c, r, z, classifier) => {
+        const v = c.p
+        if (v == -1 || v == undefined) return "na"
+        return classifier(1000000 * v / r / r)
+    },
+    color: popCols,
+})
+
