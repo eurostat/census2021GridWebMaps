876dbebc166f6556e2ec0da2b6d2887eff3cddf3
jgaffuri <jgaf@proton.me>
2026-02-05 20:47:54 +0100

    upgrade

---
+++ map_total/map_tooltip.js
@@ -0,0 +1,27 @@
+
+//tooltip functions
+const tooltipFun = (c, r) =>
+    +c[year]
+        ? "<b>" +
+        c[year] +
+        "</b> inhabitant" +
+        (+c[year] == 1 ? "" : "s") +
+        " per " +
+        (r * r) / 1000000 +
+        "km²"
+        : //+ "<br>" + c.CNTR_ID
+        undefined;
+const tooltipFunCh = (c) =>
+    "2011: " +
+    c.TOT_P_2011 +
+    "<br>2021: " +
+    c.TOT_P_2021 +
+    "<br>" +
+    "<b>" +
+    (c["d2011_2021"] == 0
+        ? "No change"
+        : (c["d2011_2021"] > 0 ? "+" : "") +
+        c["d2011_2021"] +
+        " inhabitants") +
+    "</b>";
+
