70c927dddebfe5614c5080d8439fd391b1cdf121
jgaffuri <jgaf@proton.me>
2026-02-08 19:21:18 +0100

    a

--- map_total/map_aux.js
+++ map_total/map_aux.js
@@ -1,2 +1,29 @@
 
 
+const updateURL = (map) => {
+    //get parameters
+    const p = new URLSearchParams(window.location.search);
+
+    // map viewport
+    const v = map.getView();
+    p.set("x", v.x.toFixed(0)); p.set("y", v.y.toFixed(0)); p.set("z", v.z.toFixed(0));
+
+    // handle dropdowns selection
+    p.set("year", document.getElementById("year").value);
+
+    // style parameter
+    const mapCode = document.querySelector('input[name="style"]:checked').value;
+    p.set("sty", mapCode);
+
+    // handle checkboxes
+    for (let cb of ["label", "grid", "boundary", "background"])
+        p.set(cb, document.getElementById(cb).checked ? "1" : "");
+
+    // handle background theme selection
+    p.set("bt", document.querySelector('input[name="background_theme"]:checked').value);
+
+    //set URL with map parameters
+    const newURL = `${window.location.pathname}?${p.toString()}`;
+    window.history.replaceState({}, '', newURL);
+}
+
