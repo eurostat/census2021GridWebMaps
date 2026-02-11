e5ce7a5190dba472b80c2615c9b1f7272b09ffc4
jgaffuri <jgaf@proton.me>
2026-02-11 11:49:16 +0100

    a

--- map_total/map_legends.js
+++ map_total/map_legends.js
@@ -65,6 +65,103 @@ styles.colorCh[0].legends = [
 // size change
 styles.sizeCh[0].legends = []
 
+/*
+  
+          const lgCat = new gviz.ColorCategoryLegend({
+            shape: "square",
+            colCat: [
+              ["#d13c4b", "Increase"],
+              ["#4288b5", "Decrease"],
+            ],
+          });
+
+
+//legend
+          app.layers[0].styles[0].legends.push(
+            new gviz.SizeLegend({
+              title: "Population change",
+              exaggerationFactor: 0.9,
+              shape: "circle",
+              labelUnitText: "inhabitants",
+              fillColor: "gray",
+            }).style("padding", "0px 5px")
+          );
+          app.layers[0].styles[0].legends.push(
+            new gviz.SizeLegend({
+              exaggerationFactor: 0.2,
+              shape: "circle",
+              labelUnitText: "",
+              fillColor: "gray",
+            }).style("padding", "0px 5px")
+          );
+          app.layers[0].styles[0].legends.push(
+            new gviz.SizeLegend({
+              exaggerationFactor: 0.01,
+              shape: "circle",
+              labelUnitText: "",
+              fillColor: "gray",
+            }).style("padding", "0px 5px")
+          );
+          const lgCat = new gviz.ColorCategoryLegend({
+            shape: "square",
+            colCat: [
+              ["#d13c4b", "Increase"],
+              ["#4288b5", "Decrease"],
+            ],
+          });
+          app.layers[0].styles[0].legends.push(lgCat);
+*/
+
+
+
 // segment change
 styles.segmentCh[0].legends = []
 
+/*
+          app.layers[0].styles[0].legends.push(
+            new gviz.SegmentWidthLegend({
+              title: "Population in " + year_,
+              labelUnitText: "inhab.",
+            })
+          );
+
+          app.layers[0].styles[0].legends.push(
+            new gviz.SegmentOrientationLegend({
+              title: "Population change",
+              labelUnitText: "Strong increase",
+              color: "#d13c4b",
+              orientation: 60,
+            })
+          );
+          app.layers[0].styles[0].legends.push(
+            new gviz.SegmentOrientationLegend({
+              labelUnitText: "Weak increase",
+              color: "#d13c4b",
+              orientation: 30,
+            })
+          );
+          app.layers[0].styles[0].legends.push(
+            new gviz.SegmentOrientationLegend({
+              labelUnitText: "Stability (<1%)",
+              color: "gray",
+              orientation: 0,
+            })
+          );
+          app.layers[0].styles[0].legends.push(
+            new gviz.SegmentOrientationLegend({
+              labelUnitText: "Weak decrease",
+              color: "#4288b5",
+              orientation: -30,
+            })
+          );
+          app.layers[0].styles[0].legends.push(
+            new gviz.SegmentOrientationLegend({
+              labelUnitText: "Strong decrease",
+              color: "#4288b5",
+              orientation: -60,
+            })
+          );
+          //const lgCat = new gviz.ColorCategoryLegend({ title: "Population change", shape: "square", colCat: [["#d13c4b", "Increase"], ["gray", "Stable (<1%)"], ["#4288b5", "Decrease"]] })
+          //app.layers[0].styles[0].legends.push(lgCat)
+        }
+*/
